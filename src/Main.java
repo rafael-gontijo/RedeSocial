03cd092fb54147fd5cbfa7aac35766df205351dc
rafael-gontijo <rafael-gontijo@outlook.com.br>
2023-11-22 16:48:47 -0300

    Menu Console Atualizado

--- src/Main.java
+++ src/Main.java
@@ -9,85 +9,88 @@ public class Main {
             System.out.println("\n===== Mini Simulador de Rede Social =====");
             if (sistema.getUsuarioLogado() != null) {
                 System.out.println("Bem-vindo, " + sistema.getUsuarioLogado().getNome() + "!");
-                System.out.println("1. Cadastrar Usuário");
-                System.out.println("2. Login");
-                System.out.println("3. Adicionar Amigo");
-                System.out.println("4. Consultar Amigos");
-                System.out.println("5. Enviar Mensagem");
-                System.out.println("6. Visualizar Mensagens Enviadas");
-                System.out.println("7. Visualizar Mensagens Recebidas");
-                System.out.println("8. Logout");
+                System.out.println("1. Adicionar Amigo");
+                System.out.println("2. Consultar Amigos");
+                System.out.println("3. Enviar Mensagem");
+                System.out.println("4. Visualizar Mensagens Enviadas");
+                System.out.println("5. Visualizar Mensagens Recebidas");
+                System.out.println("6. Logout");
                 System.out.println("0. Sair");
+                System.out.print("Escolha uma opção: ");
+                int opcao = scanner.nextInt();
+                scanner.nextLine();
+                switch (opcao) {
+                    case 1:
+                        if (sistema.getUsuarioLogado() != null) {
+                            System.out.print("Email do Amigo: ");
+                            String emailAmigo = scanner.nextLine();
+                            sistema.adicionarAmigo(emailAmigo);
+                        } else {
+                            System.out.println("Você precisa estar logado para adicionar amigos.");
+                        }
+                        break;
+                    case 2:
+                        sistema.consultarAmigos();
+                        break;
+                    case 3:
+                        if (sistema.getUsuarioLogado() != null) {
+                            System.out.print("Email do Destinatário: ");
+                            String emailDestinatario = scanner.nextLine();
+                            System.out.print("Conteúdo da Mensagem: ");
+                            String conteudoMensagem = scanner.nextLine();
+                            sistema.enviarMensagem(emailDestinatario, conteudoMensagem);
+                        } else {
+                            System.out.println("Você precisa estar logado para enviar mensagens.");
+                        }
+                        break;
+                    case 4:
+                        sistema.visualizarMensagensEnviadas();
+                        break;
+                    case 5:
+                        sistema.visualizarMensagensRecebidas();
+                        break;
+                    case 6:
+                        sistema.logout();
+                        break;
+                    case 0:
+                        System.out.println("Saindo do programa. Até mais!");
+                        System.exit(0);
+                    default:
+                        System.out.println("Opção inválida. Tente novamente.");
+
+
+                }
             } else {
                 System.out.println("1. Cadastrar Usuário");
                 System.out.println("2. Login");
-                System.out.println("3. Adicionar Amigo");
-                System.out.println("4. Consultar Amigos");
-                System.out.println("5. Enviar Mensagem");
-                System.out.println("6. Visualizar Mensagens Enviadas");
-                System.out.println("7. Visualizar Mensagens Recebidas");
                 System.out.println("0. Sair");
-            }
+                System.out.print("Escolha uma opção: ");
+                int opcao = scanner.nextInt();
+                scanner.nextLine();
+                switch (opcao) {
+                    case 1:
+                        System.out.print("Nome: ");
+                        String nome = scanner.nextLine();
+                        System.out.print("Email: ");
+                        String email = scanner.nextLine();
+                        System.out.print("Senha: ");
+                        String senha = scanner.nextLine();
+                        sistema.cadastrarUsuario(nome, email, senha);
+                        break;
+                    case 2:
+                        System.out.print("Email: ");
+                        String emailLogin = scanner.nextLine();
+                        System.out.print("Senha: ");
+                        String senhaLogin = scanner.nextLine();
+                        sistema.login(emailLogin, senhaLogin);
+                        break;
+                    case 0:
+                        System.out.println("Saindo do programa. Até mais!");
+                        System.exit(0);
+                    default:
+                        System.out.println("Opção inválida. Tente novamente.");
 
-
-            System.out.print("Escolha uma opção: ");
-            int opcao = scanner.nextInt();
-            scanner.nextLine();
-
-            switch (opcao) {
-                case 1:
-                    System.out.print("Nome: ");
-                    String nome = scanner.nextLine();
-                    System.out.print("Email: ");
-                    String email = scanner.nextLine();
-                    System.out.print("Senha: ");
-                    String senha = scanner.nextLine();
-                    sistema.cadastrarUsuario(nome, email, senha);
-                    break;
-                case 2:
-                    System.out.print("Email: ");
-                    String emailLogin = scanner.nextLine();
-                    System.out.print("Senha: ");
-                    String senhaLogin = scanner.nextLine();
-                    sistema.login(emailLogin, senhaLogin);
-                    break;
-                case 3:
-                    if (sistema.getUsuarioLogado() != null) {
-                        System.out.print("Email do Amigo: ");
-                        String emailAmigo = scanner.nextLine();
-                        sistema.adicionarAmigo(emailAmigo);
-                    } else {
-                        System.out.println("Você precisa estar logado para adicionar amigos.");
-                    }
-                    break;
-                case 4:
-                    sistema.consultarAmigos();
-                    break;
-                case 5:
-                    if (sistema.getUsuarioLogado() != null) {
-                        System.out.print("Email do Destinatário: ");
-                        String emailDestinatario = scanner.nextLine();
-                        System.out.print("Conteúdo da Mensagem: ");
-                        String conteudoMensagem = scanner.nextLine();
-                        sistema.enviarMensagem(emailDestinatario, conteudoMensagem);
-                    } else {
-                        System.out.println("Você precisa estar logado para enviar mensagens.");
-                    }
-                    break;
-                case 6:
-                    sistema.visualizarMensagensEnviadas();
-                    break;
-                case 7:
-                    sistema.visualizarMensagensRecebidas();
-                    break;
-                case 8:
-                    sistema.logout();
-                    break;
-                case 0:
-                    System.out.println("Saindo do programa. Até mais!");
-                    System.exit(0);
-                default:
-                    System.out.println("Opção inválida. Tente novamente.");
+                }
             }
         }
     }
